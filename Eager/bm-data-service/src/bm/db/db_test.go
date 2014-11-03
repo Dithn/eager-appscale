4b6df5ef7f9448d49c64abb5e61a899bd3076673
Hiranya Jayathilaka <hiranya911@gmail.com>
2014-11-03 11:39:11 -0800

    source formatting

--- Eager/bm-data-service/src/bm/db/db_test.go
+++ Eager/bm-data-service/src/bm/db/db_test.go
@@ -9,20 +9,20 @@ func TestFSDatabase(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-	
+
 	got, err := fsd.Query(10, []string{"Test1()", "Test2()", "Test3()"})
 	if err != nil {
 		t.Error(err)
 	}
 
-	for _, want := range []struct{
+	for _, want := range []struct {
 		op string
 		ts TimeSeries
 	}{
-		{op: "Test1()", ts: TimeSeries([]int{3,4,5,6,7,8,9,10,11,12})},
-		{op: "Test2()", ts: TimeSeries([]int{5,4,3,2,1})},
-		{op: "Test3()", ts: TimeSeries([]int{1,2,3,4,5,6,7,8,9,10})},
-	}{
+		{op: "Test1()", ts: TimeSeries([]int{3, 4, 5, 6, 7, 8, 9, 10, 11, 12})},
+		{op: "Test2()", ts: TimeSeries([]int{5, 4, 3, 2, 1})},
+		{op: "Test3()", ts: TimeSeries([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10})},
+	} {
 		if !compareTS(got[want.op], want.ts) {
 			t.Errorf("incorrect query results; want: %v, got %v", want.ts, got[want.op])
 		}
@@ -37,7 +37,7 @@ func compareTS(ts1, ts2 TimeSeries) bool {
 	if len(ts1) != len(ts2) {
 		return false
 	}
-	for i,v := range ts1 {
+	for i, v := range ts1 {
 		if v != ts2[i] {
 			return false
 		}
--- Eager/bm-data-service/src/bm/qbets/qbets_test.go
+++ Eager/bm-data-service/src/bm/qbets/qbets_test.go
@@ -8,7 +8,7 @@ import (
 func TestPredictQuantile(t *testing.T) {
 	slice := make([]int, 0, 1000)
 	for i := 0; i < 1000; i++ {
-		slice = append(slice, i % 100)
+		slice = append(slice, i%100)
 	}
 	q, err := PredictQuantile(db.TimeSeries(slice), 0.95, 0.05)
 	if err != nil {
