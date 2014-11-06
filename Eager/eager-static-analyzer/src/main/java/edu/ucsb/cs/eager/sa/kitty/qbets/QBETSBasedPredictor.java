8fdaade7b91fa9a8b5c7ebd6fb8382d19462d7af
Hiranya Jayathilaka <hiranya911@gmail.com>
2014-11-06 15:36:25 -0800

    implemented quantile cache

--- Eager/eager-static-analyzer/src/main/java/edu/ucsb/cs/eager/sa/kitty/qbets/QBETSBasedPredictor.java
+++ Eager/eager-static-analyzer/src/main/java/edu/ucsb/cs/eager/sa/kitty/qbets/QBETSBasedPredictor.java
@@ -39,27 +39,31 @@ import java.util.*;
 
 public class QBETSBasedPredictor {
 
-    public static void predict(PredictionConfig config, Collection<MethodInfo> methods) throws IOException {
+    public static void predict(PredictionConfig config,
+                               Collection<MethodInfo> methods) throws IOException {
         if (config.isAggregateTimeSeries()) {
             throw new NotImplementedException();
         }
 
         System.out.printf("\nQuantile: %.4f; Confidence: %.4f\n", config.getQuantile(),
                 config.getConfidence());
+        QuantileCache cache = new QuantileCache();
         for (MethodInfo m : methods) {
-            Prediction prediction = predictExecTime(m, config);
+            Prediction prediction = predictExecTime(m, config, cache);
             System.out.printf("%s\t%d\t%s\n", m.getName(), m.getPaths().size(), prediction.toString());
         }
     }
 
-    private static Prediction predictExecTime(MethodInfo method,
-                                              PredictionConfig config) throws IOException {
+    private static Prediction predictExecTime(MethodInfo method, PredictionConfig config,
+                                              QuantileCache cache) throws IOException {
 
         List<List<APICall>> pathsOfInterest = new ArrayList<List<APICall>>();
         for (List<APICall> p : method.getPaths()) {
             if (p.size() == 1 && p.get(0).getName().equals("-- No API Calls --")) {
+                // this is for when the trace is loaded from a file
                 continue;
             } else if (p.size() == 0) {
+                // this is for when the trace is loaded from Cerebro
                 continue;
             }
             pathsOfInterest.add(p);
@@ -79,17 +83,30 @@ public class QBETSBasedPredictor {
         }
 
         // Compute quantiles for each API call, under different API call counts
-        Map<Integer, Map<String,Integer>> quantiles = new HashMap<Integer, Map<String, Integer>>();
         for (Integer pathLength : pathLengths) {
+            Set<String> reducedOps = new HashSet<String>();
+            for (String op : uniqueOps) {
+                if (!cache.contains(op, pathLength)) {
+                    reducedOps.add(op);
+                }
+            }
+
+            if (reducedOps.isEmpty()) {
+                continue;
+            }
+
             double adjustedQuantile = Math.pow(config.getQuantile(), 1.0/pathLength);
-            quantiles.put(pathLength, getQuantiles(config.getBenchmarkDataSvc(),
-                    adjustedQuantile, config.getConfidence(), uniqueOps));
+            Map<String,Integer> map = getQuantiles(config.getBenchmarkDataSvc(),
+                    adjustedQuantile, config.getConfidence(), reducedOps);
+            for (Map.Entry<String,Integer> entry : map.entrySet()) {
+                cache.put(entry.getKey(), pathLength, entry.getValue());
+            }
         }
 
         Prediction[] predictions = new Prediction[pathsOfInterest.size()];
         for (int i = 0; i < pathsOfInterest.size(); i++) {
             List<APICall> path = pathsOfInterest.get(i);
-            predictions[i] = analyzePath(path, quantiles.get(path.size()));
+            predictions[i] = analyzePath(path, cache);
         }
 
         // And return the most expensive one
@@ -102,16 +119,18 @@ public class QBETSBasedPredictor {
         return max;
     }
 
-    private static Prediction analyzePath(List<APICall> path, Map<String,Integer> quantiles) {
+    private static Prediction analyzePath(List<APICall> path, QuantileCache cache) {
         double total = 0.0;
         for (APICall call : path) {
-            total += quantiles.get(call.getShortName());
+            total += cache.get(call.getShortName(), path.size());
         }
         return new Prediction(total);
     }
 
-    private static Map<String,Integer> getQuantiles(String bmDataSvc, double quantile,
-                                                    double confidence, Collection<String> ops) throws IOException {
+    private static Map<String,Integer> getQuantiles(String bmDataSvc,
+                                                    double quantile,
+                                                    double confidence,
+                                                    Collection<String> ops) throws IOException {
         JSONObject msg = new JSONObject();
         msg.put("quantile", quantile);
         msg.put("confidence", confidence);
