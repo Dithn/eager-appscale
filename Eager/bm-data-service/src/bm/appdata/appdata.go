e94036b7828bbd8759a013f43c3e27f1bab5a3a0
Hiranya Jayathilaka <hiranya911@gmail.com>
2014-11-07 13:11:10 -0800

    program to analyze benchmark data from apps

---
+++ Eager/bm-data-service/src/bm/appdata/appdata.go
@@ -0,0 +1,57 @@
+package main 
+
+import (
+	"bm/db"
+	"bm/qbets"
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	path := flag.String("p", "", "Path to the input file")
+	q := flag.Float64("q", 0.95, "Quantile to predict")
+	c := flag.Float64("c", 0.05, "Upper confidence of the prediction")
+
+	flag.Parse()
+	if *path == "" {
+		fmt.Println("input file path not specified")
+		return
+	}
+	
+	file, err := os.Open(*path)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	var ts db.TimeSeries
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		segments := strings.Fields(line)
+		val, err := strconv.ParseFloat(segments[1], 64)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		ts = append(ts, int(val * 1000.0))
+	}
+	fmt.Println("Loaded", len(ts), "data points from file")
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	pred, err := qbets.PredictQuantile(ts, *q, *c)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Prediction:", pred)
+}
