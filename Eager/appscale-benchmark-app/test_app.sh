ad6f66a555fa36a1642d324d3a6ed9e48187f737
Hiranya Jayathilaka <hiranya911@gmail.com>
2014-11-13 18:31:49 -0800

    updated results sheet

--- Eager/appscale-benchmark-app/test_app.sh
+++ Eager/appscale-benchmark-app/test_app.sh
@@ -6,8 +6,9 @@ rm results.txt
 while [ $c -le $2 ]
 do
   echo "Iteration $c"
-  curl -w "@curl-format.txt" -o /dev/null -s $1 | grep "time_total" >> results.txt
+  #curl -w "@curl-format.txt" -o /dev/null -s $1 | grep "time_total" >> results.txt
   #curl -w "@curl-format.txt" -o /dev/null -X POST -d "comment=Foo" -s $1 | grep "time_total" >> results.txt
+  curl -w "@curl-format.txt" -o /dev/null -X POST -d "firstName=Foo&lastName=Bar" -s $1 | grep "time_total" >> results.txt
   sleep 1
   (( c++ ))
 done
