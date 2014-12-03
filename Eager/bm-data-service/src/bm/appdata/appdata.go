ad1e63ad10cd031695c62697865791bc463101b5
Hiranya Jayathilaka <hiranya911@gmail.com>
2014-12-03 15:24:21 -0800

    added latest results from benchmarking

--- Eager/bm-data-service/src/bm/appdata/appdata.go
+++ Eager/bm-data-service/src/bm/appdata/appdata.go
@@ -6,16 +6,19 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"math"
 	"os"
 	"sort"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 func main() {
 	path := flag.String("p", "", "Path to the input file")
 	q := flag.Float64("q", 0.95, "Quantile to predict")
 	c := flag.Float64("c", 0.05, "Upper confidence of the prediction")
+	tr := flag.Bool("t", false, "Do a trace analysis")
 
 	flag.Parse()
 	if *path == "" {
@@ -49,7 +52,12 @@ func main() {
 		return
 	}
 
-	pred, err := qbets.PredictQuantile(ts, *q, *c)
+	if *tr {
+		analyzeTrace(ts, *q, *c)
+		fmt.Println()
+	}
+
+	pred, err := qbets.PredictQuantile(ts, *q, *c, false)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -59,4 +67,49 @@ func main() {
 	sort.Ints(ts)
 	index := int(float64(len(ts)) * (*q))
 	fmt.Println("Actual Quantile: ", ts[index - 1], "(element =", index, ")")
+}
+
+func analyzeTrace(ts []int, q, c float64) {
+	minIndex := int(math.Log(c) / math.Log(q)) + 10
+	fmt.Println("Min index: ", minIndex)
+	if len(ts) < minIndex + 1 {
+		fmt.Println("insufficient data in time series")
+		return
+	}
+
+	results := make([]int, len(ts) - minIndex, len(ts) - minIndex)
+	var perr error
+	workers := make(chan bool, 8)
+	var wg sync.WaitGroup
+
+	for i := minIndex; i < len(ts); i++ {
+		workers <- true
+		wg.Add(1)
+		go func(cnt int) {
+			defer func(){ <- workers }()
+			defer wg.Done()
+			if perr != nil {
+				return
+			}
+			data := ts[0:cnt+1]
+			pred, err := qbets.PredictQuantile(data, q, c, false)
+			if err != nil {
+				perr = err
+				return
+			}
+			results[cnt - minIndex] = int(pred)
+			if cnt % 100 == 0 {
+				fmt.Println("Computed the predictions for index:", cnt)
+			}
+		}(i)
+	}
+	wg.Wait()
+
+	if perr != nil {
+		fmt.Println(perr)
+		return
+	}
+	for i := minIndex; i < len(results); i++ {
+		fmt.Printf("[trace] %d %d %d\n", i, results[i], ts[i])
+	}
 }
