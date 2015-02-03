c7b3b848183f565d1f6c107109bbf25a17f2453b
Hiranya Jayathilaka <hiranya911@gmail.com>
2015-02-02 18:02:58 -0800

    support for dumping time series data

--- Eager/bm-data-service/src/bm/dbdump/dbdump.go
+++ Eager/bm-data-service/src/bm/dbdump/dbdump.go
@@ -13,6 +13,7 @@ func main() {
 	c := flag.Float64("c", 0.05, "Upper confidence of the prediction")
 	op := flag.String("o", "", "Operation for which the quantile should be predicted")
 	l := flag.Int("l", -1, "Length of the time series to process")
+	t := flag.Bool("t", false, "Dump the time series data without making predictions")
 	flag.Parse()
 	if *url == "" {
 		fmt.Println("URL of the Watchtower service not specified.")
@@ -28,12 +29,34 @@ func main() {
 	}
 	fmt.Println("Loading TimeSeries data from the Watchtower service at", d.BaseURL)
 
-	p, err := getQuantile(d, *q, *c, *op, *l)
-	if err != nil {
-		fmt.Println(err)
-		return
+	if *t {
+		if err := dump(d, *op, *l); err != nil {
+			fmt.Println(err)
+		}
+	} else {
+		p, err := getQuantile(d, *q, *c, *op, *l)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Printf("Predicted %.2f Quantile: %d\n", *q, p)
 	}
-	fmt.Printf("Predicted %.2f Quantile: %d\n", *q, p)
+}
+
+func dump(d db.Database, op string, l int) error {
+	result, err := d.Query(-1, []string{op}, -1, -1)
+	if err != nil {
+		return err
+	}
+
+	ts := result[op]
+	if l > 0 {
+		ts = ts[0:l]
+	}
+	for _,v := range ts {
+		fmt.Println(v.Timestamp, v.Value)
+	}
+	return nil
 }
 
 func getQuantile(d db.Database, q, c float64, op string, l int) (int,error) {
