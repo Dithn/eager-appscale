53733eafd9bd50771143b053227cbd0e13a6a894
Hiranya Jayathilaka <hiranya911@gmail.com>
2015-04-29 15:05:27 -0700

    script for generating cdf from 2column data

---
+++ Eager/eager-static-analyzer/results/apps/cdf_2col.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"sort"
+	"strings"
+	"strconv"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: cdf_2col <path>")
+		return
+	}
+
+	inputFile, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Println("Error while opening input file", err)
+		return
+	}
+
+	defer inputFile.Close()
+	bf := bufio.NewReader(inputFile)
+	data := make(map[float64]int)
+	for {
+		l, isPrefix, err := bf.ReadLine()
+                if err == io.EOF {
+			break
+		} else if err != nil {
+			fmt.Println("Error while reading line", err)
+			return
+		} else if isPrefix {
+			fmt.Println("Unexpected long line", err)
+			return
+		}
+
+		fields := strings.Fields(string(l))
+		var key float64
+		if fields[0] == "---" {
+			key = 0.0
+		} else {
+			key, err = strconv.ParseFloat(fields[0], 64)
+			if err != nil {
+				fmt.Println("Error while parsing string", err)
+				return
+			}
+		}
+
+		val, err := strconv.Atoi(fields[1])
+		if err != nil {
+			fmt.Println("Error while parsing string", err)
+			return
+		}
+		data[key] += val
+	}
+
+	var keys []float64
+	total := 0
+	for k, v := range data {
+		keys = append(keys, k)
+		total += v
+	}
+	sort.Float64s(keys)
+	
+	cumulative := 0
+	for _,k := range keys {
+		cumulative += data[k]
+		fmt.Printf("%.4f %.4f\n", k, float64(cumulative)/float64(total))
+	}
+}
