7f0c63fdd0d5d80d94763370c8158652b1676955
Hiranya Jayathilaka <hiranya911@gmail.com>
2015-11-08 00:08:20 -0800

    Cleaned up the mkfs code

--- Eager/cs270/mkfs_h2.c
+++ Eager/cs270/mkfs_h2.c
@@ -10,18 +10,25 @@ int main(int agrc, char** argv) {
   char *block_device = argv[1];
   float inode_percentage = atof(argv[2]);
   long total_num_bytes = atol(argv[3]);
-  /*number of blocks on disk*/
+
+  /* number of blocks on disk */
   long block_count = total_num_bytes / BLOCK_SIZE;
+  if (block_count == 0) {
+    printf("Not enough space on the device for installing a file system\n");
+    return 1;
+  }
+
   long inode_block_count = inode_percentage * block_count;
   if (inode_block_count == 0) {
-    printf("Not enough space\n");
+    printf("Not enough blocks available for inodes\n");
     return 1;
   }
 
   /* number of i-nodes in the FS */
-  long inode_count = inode_block_count*BLOCK_SIZE/sizeof(inode);
+  long inode_count = inode_block_count * BLOCK_SIZE / sizeof(inode);
+
   /* number of blocks for a bit map */
-  long bit_map_size_in_blocks = ceil(ceil(inode_count/sizeof(char))/BLOCK_SIZE); 
+  long bit_map_blocks = ceil(ceil(inode_count / sizeof(char)) / BLOCK_SIZE); 
 
   if (open_device(block_device) < 0) {
     printf("Failed to open device: %s\n", block_device);
@@ -32,7 +39,7 @@ int main(int agrc, char** argv) {
   memset(bit_map, 0, BLOCK_SIZE);  
   int i;
   block_id current = 1;
-  for (i = 0; i < bit_map_size_in_blocks; i++) {
+  for (i = 0; i < bit_map_blocks; i++) {
     printf("Writing inode bitmap at block %d\n", current);
     write_block(current++, bit_map, BLOCK_SIZE);
   }
@@ -48,7 +55,7 @@ int main(int agrc, char** argv) {
   free(inodes);
 
   long remaining = block_count - current;
-  printf("Blocks available for data: %ld\n", remaining);
+  printf("\nBlocks available for data: %ld\n", remaining);
   init_free_list(current, remaining);
   if (check_free_list(current, remaining) != 1) {
     printf("Problem in creating free list\n");
@@ -62,10 +69,8 @@ int main(int agrc, char** argv) {
   sb->ilist_length = inode_count;
   sb->freelist_head = current;
   write_block(0, sb, sizeof(superblock));
+  free(sb);
 
-  printf("inode count: %ld, bit map size in blocks: %ld \n", inode_count, bit_map_size_in_blocks);
-  printf("size of inode: %ld \n", sizeof(inode));
-  printf("block_count: %ld, inode percentage: %f, total bytes: %ld\n", block_count, inode_percentage, total_num_bytes);
   close_device();
   return 0;
 }
