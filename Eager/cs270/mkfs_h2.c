e3d6e05718d2e1ac845863c52a21f96f67f7d4cc
Hiranya Jayathilaka <hiranya911@gmail.com>
2015-11-08 15:38:19 -0800

    Code cleanup and build-all make rule

--- Eager/cs270/mkfs_h2.c
+++ Eager/cs270/mkfs_h2.c
@@ -6,7 +6,12 @@
 #include "ilist.h"
 #include "block_io.h"
 
-int main(int agrc, char** argv) {
+int main(int argc, char** argv) {
+  if (argc != 4) {
+    printf("Usage: mkfs.h2 device_path inode_percentage total_size\n");
+    return 1;
+  }
+
   char *block_device = argv[1];
   float inode_percentage = atof(argv[2]);
   long total_num_bytes = atol(argv[3]);
@@ -27,23 +32,23 @@ int main(int agrc, char** argv) {
   /* number of i-nodes in the FS */
   long inode_count = inode_block_count * BLOCK_SIZE / sizeof(inode);
 
-  /* number of blocks for a bit map */
-  long bit_map_blocks = ceil(ceil(inode_count / sizeof(char)) / BLOCK_SIZE); 
+  /* number of blocks for a bitmap */
+  long bitmap_blocks = ceil(ceil(inode_count / sizeof(char)) / BLOCK_SIZE); 
 
   if (open_device(block_device) < 0) {
     printf("Failed to open device: %s\n", block_device);
     return 1;
   }
 
-  int *bit_map = malloc(BLOCK_SIZE);
-  memset(bit_map, 0, BLOCK_SIZE);  
-  int i;
+  int *bitmap = malloc(BLOCK_SIZE);
+  memset(bitmap, 0, BLOCK_SIZE);  
+  long i;
   block_id current = 1;
-  for (i = 0; i < bit_map_blocks; i++) {
+  for (i = 0; i < bitmap_blocks; i++) {
     printf("Writing inode bitmap at block %d\n", current);
-    write_block(current++, bit_map, BLOCK_SIZE);
+    write_block(current++, bitmap, BLOCK_SIZE);
   }
-  free(bit_map);
+  free(bitmap);
 
   block_id ilist_head = current;
   inode *inodes = malloc(BLOCK_SIZE);
@@ -68,9 +73,11 @@ int main(int agrc, char** argv) {
   sb->ilist = ilist_head;
   sb->ilist_length = inode_count;
   sb->freelist_head = current;
+  printf("\nWriting superblock\n");
   write_block(0, sb, sizeof(superblock));
   free(sb);
 
+  printf("H2 file system initialized successfully\n");
   close_device();
   return 0;
 }
