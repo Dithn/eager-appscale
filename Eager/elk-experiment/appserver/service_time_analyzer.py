f7a05b4e949679521dddd6d4724caf93a79943e3
Hiranya Jayathilaka <hiranya911@gmail.com>
2015-12-10 21:30:52 -0800

    Output display from the service time analyzer

--- Eager/elk-experiment/appserver/service_time_analyzer.py
+++ Eager/elk-experiment/appserver/service_time_analyzer.py
@@ -19,20 +19,16 @@ def get_request_info(server, port, index, app, time_window):
       'sort': { 'timestamp' : { 'order' : 'asc'}},
       'aggs': {
         'group_by_request' : {
-          'terms' : {
-            'field' : 'requestId.raw',
-            'size': 0
-          },
+          'terms' : { 'field' : 'requestId.raw', 'size': 0, 'order': {'request_timestamp': 'asc'} },
           'aggs': {
+            'request_timestamp': {
+              'min': { 'field': 'timestamp'}
+            },
             'group_by_service': {
-              'terms': {
-                'field': 'service.raw'
-              },
+              'terms': { 'field': 'service.raw' },
               'aggs': {
                 'service_time': {
-                  'sum': {
-                    'field': 'elapsed'
-                  }
+                  'sum': { 'field': 'elapsed' }
                 }
               }
             }
@@ -41,15 +37,38 @@ def get_request_info(server, port, index, app, time_window):
       }
     }
 
-    path = '/{0}/apicall/_search?pretty'.format(index)
+    path = '/{0}/apicall/_search'.format(index)
     conn = httplib.HTTPConnection(server, port)
     conn.request('POST', path, json.dumps(query))
     response = conn.getresponse()
-    print 'Operation completed with status', response.status
     data = response.read()
-    print data
     conn.close()
-    
+    if response.status != 200:
+        print 'Server returned unexpected status', response.status
+        print data
+        return
+    output = json.loads(data)
+    requests = output['aggregations']['group_by_request']['buckets']
+    service_names = [ 'datastore_v3', 'memcache', 'urlfetch' ]
+    if not requests:
+        print 'No requests found'
+        return
+    print 'requestId  datastore_v3 datastore_v3%  memcache memcache%  urlfetch urlfetch%  total'
+    for req in requests:
+        services = req['group_by_service']['buckets']
+        service_times = {}
+        for service in services:
+            name = service['key']
+            value = service['service_time']['value']
+            service_times[name] = value
+        total = sum(service_times.values())
+        record = '{0}  {1}  '.format(req['request_timestamp']['value_as_string'], req['key'])
+        for k in service_names:
+            value = service_times.get(k, 0.0)
+            record += '{0}  ({1:.2f})  '.format(value, (value/total) * 100.0)
+        record += '{0}'.format(total)
+        print record
+        
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Analyzes execution time of cloud services.')
