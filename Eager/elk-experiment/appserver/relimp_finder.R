8d2d4c2c3b9c5e4d2f9619bf396c2e6ad174754b
Hiranya Jayathilaka <hiranya911@gmail.com>
2016-02-19 14:00:13 -0800

    Relimp plotter script

--- Eager/elk-experiment/appserver/relimp_finder.R
+++ Eager/elk-experiment/appserver/relimp_finder.R
@@ -8,23 +8,23 @@ limit2 <- quantile(gae$bm_datastore_asList_1000, 0.95)
 model <- lm(Total ~ ., data=gae)
 rankings <- calc.relimp(model, type=c('lmg'))
 lmgr <- rankings$lmg
-cat('[relimp]', names(lmgr), '\n')
+cat('[relimp] index', names(lmgr), '\n')
 
 anomalies <- 0
 condition_met <- 0
-for (i in 1:nrow(gae)) {
+for (i in 1:2000) {
+  if (i > 100) {
+    model <- lm(Total ~ ., data=gae[1:i,])
+    rankings <- calc.relimp(model, type=c('lmg'))
+    lmgr <- rankings$lmg
+    cat('[relimp]', i, paste(lmgr, ' '), '\n')
+  }
   if (limit < gae[i, "Total"]) {
     anomalies <- anomalies + 1
     if (limit2 < gae[i, "bm_datastore_asList_1000"]) {
       condition_met <- condition_met + 1
     }
-    cat('[info]', i, gae[i, "Total"], gae[i, "bm_datastore_asList_1000"], gae[i, "bm_datastore_asList_1000"] > limit2, "\n")
-    if (i > 100) {
-      model <- lm(Total ~ ., data=gae[1:i,])
-      rankings <- calc.relimp(model, type=c('lmg'))
-      lmgr <- rankings$lmg
-      cat('[relimp]', paste(lmgr, ' '), '\n')
-    }
+    cat('[anomaly]', i, gae[i, "Total"], gae[i, "bm_datastore_asList_1000"], gae[i, "bm_datastore_asList_1000"] > limit2, "\n")
   }
 }
 
---
+++ Eager/elk-experiment/appserver/relimp_plotter.R
@@ -0,0 +1,19 @@
+library('methods')
+
+relimp_vs_time <- read.table('gae_relimp_over_time.txt', header=T)
+anomaly_vs_time <- read.table('gae_anomaly_over_time.txt')
+
+columns = c('bm_datastore_asList_1000', 'bm_datastore_asList_100', 'bm_datastore_asList_10', 'bm_datastore_asSingleEntity', 'bm_datastore_get', 'bm_datastore_put', 'bm_datastore_delete', 'bm_memcache_get', 'bm_memcache_put', 'bm_memcache_delete')
+cl <- rainbow(10)
+
+x <- relimp_vs_time$index
+plot(x, relimp_vs_time$bm_datastore_asList_1000, type='n', ylim=c(0,1), xlab='Time', ylab='Relative Importance')
+for (i in 1:10) {
+  lines(x, relimp_vs_time[[columns[i]]], type='l', col=cl[i])
+}
+
+#legend('middleright', legend=)
+legend("topright", legend = columns, col = cl, lwd = 1, cex = 0.5)
+for (i in 1:nrow(anomaly_vs_time)) {
+  abline(v=anomaly_vs_time[i,2])
+}
