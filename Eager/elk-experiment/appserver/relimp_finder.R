8424851d6940c5702ea47bb6b4984f4081320c81
Hiranya Jayathilaka <hiranya911@gmail.com>
2016-02-18 16:33:34 -0800

    Iterative relimp finder in R

--- Eager/elk-experiment/appserver/relimp_finder.R
+++ Eager/elk-experiment/appserver/relimp_finder.R
@@ -1,5 +1,19 @@
 library('relaimpo')
+library('methods')
 
 gae <- read.table('gae_5d_harvest.txt', header=TRUE)
 gae_model <- lm(Total ~ ., data=gae)
 calc.relimp(gae_model, type=c('lmg'))
+
+limit <- quantile(gae$Total, 0.95)
+for (i in 1:nrow(gae)) {
+  if (limit < gae[i, "Total"]) {
+    cat(i, gae[i, "Total"], '\n')
+    if (i > 100) {
+      model <- lm(Total ~ ., data=gae[1:i,])
+      rankings <- calc.relimp(model, type=c('lmg'))
+      print(rankings$lmg)
+    }
+    cat('\n')
+  }
+}
