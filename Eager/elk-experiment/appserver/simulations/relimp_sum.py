f72da7007aabb44ba5e2b26e6cee54dc5f9e77e2
hiranya <hiranya911@gmail.com>
2016-03-06 19:20:47 -0800

    Minimum relimp total

--- Eager/elk-experiment/appserver/simulations/relimp_sum.py
+++ Eager/elk-experiment/appserver/simulations/relimp_sum.py
@@ -5,7 +5,12 @@ if __name__ == '__main__':
     lines = fp.readlines()
     fp.close()
 
+    minimum = 1.0
     for line in lines[1:]:
-        print sum([float(x) for x in line.split()[2:]])
-        
+        total = sum([float(x) for x in line.split()[2:]])
+        print total
+        if total < minimum:
+            minimum = total
+    print
+    print 'Smallest total relimp: {0}'.format(minimum)
         
