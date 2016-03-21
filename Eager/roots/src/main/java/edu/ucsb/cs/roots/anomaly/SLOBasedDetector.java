a69e48d2ad4b5770b5b51f6c2de059e0bba6c1af
hiranya <hiranya911@gmail.com>
2016-03-20 19:44:21 -0700

    Fixed the SLO based detector to use a fixed time window

--- Eager/roots/src/main/java/edu/ucsb/cs/roots/anomaly/SLOBasedDetector.java
+++ Eager/roots/src/main/java/edu/ucsb/cs/roots/anomaly/SLOBasedDetector.java
@@ -1,20 +1,19 @@
 package edu.ucsb.cs.roots.anomaly;
 
-import com.google.common.collect.EvictingQueue;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import edu.ucsb.cs.roots.data.AccessLogEntry;
+import edu.ucsb.cs.roots.utils.ImmutableCollectors;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
 public final class SLOBasedDetector extends AnomalyDetector {
 
-    private final int historyLength;
-    private final Map<String,EvictingQueue<AccessLogEntry>> history;
+    private final int historyLengthInSeconds;
+    private final int samplingIntervalInSeconds;
+    private final Map<String,List<AccessLogEntry>> history;
     private final int responseTimeUpperBound;
     private final double sloPercentage;
 
@@ -22,12 +21,16 @@ public final class SLOBasedDetector extends AnomalyDetector {
 
     private SLOBasedDetector(Builder builder) {
         super(builder.application, builder.periodInSeconds, builder.dataStore);
-        checkArgument(builder.historyLength > 10, "History length must be greater than 10");
+        checkArgument(builder.historyLengthInSeconds > 0, "History length must be positive");
+        checkArgument(builder.samplingIntervalInSeconds > 0, "Sampling interval must be positive");
+        checkArgument(builder.historyLengthInSeconds > builder.samplingIntervalInSeconds,
+                "History length must be larger than sampling interval");
         checkArgument(builder.responseTimeUpperBound > 0,
                 "Response time upper bound must be positive");
         checkArgument(builder.sloPercentage > 0 && builder.sloPercentage < 100,
                 "SLO percentage must be in the interval (0,100)");
-        this.historyLength = builder.historyLength;
+        this.historyLengthInSeconds = builder.historyLengthInSeconds;
+        this.samplingIntervalInSeconds = builder.samplingIntervalInSeconds;
         this.history = new HashMap<>();
         this.responseTimeUpperBound = builder.responseTimeUpperBound;
         this.sloPercentage = builder.sloPercentage;
@@ -47,18 +50,29 @@ public final class SLOBasedDetector extends AnomalyDetector {
         ImmutableMap<String,List<AccessLogEntry>> summaries = dataStore.getBenchmarkResults(
                 application, start, end);
         for (Map.Entry<String,List<AccessLogEntry>> entry : summaries.entrySet()) {
-            EvictingQueue<AccessLogEntry> record = history.get(entry.getKey());
+            List<AccessLogEntry> record = history.get(entry.getKey());
             if (record == null) {
-                record = EvictingQueue.create(historyLength);
+                record = new ArrayList<>();
                 history.put(entry.getKey(), record);
             }
             record.addAll(entry.getValue());
         }
 
+        long cutoff = end - historyLengthInSeconds * 1000;
+        history.values().forEach(v -> cleanupOldData(cutoff, v));
+
+        int maxSamples = historyLengthInSeconds / samplingIntervalInSeconds;
         history.entrySet().stream()
                 .filter(e -> summaries.containsKey(e.getKey()) &&
-                        e.getValue().size() == historyLength)
+                        e.getValue().size() > maxSamples * 0.95)
                 .forEach(e -> computeSLO(e.getKey(), e.getValue()));
+    }
+
+    private void cleanupOldData(long cutoff, List<AccessLogEntry> summaries) {
+        ImmutableList<AccessLogEntry> oldData = summaries.stream()
+                .filter(s -> s.getTimestamp() < cutoff)
+                .collect(ImmutableCollectors.toList());
+        oldData.forEach(summaries::remove);
     }
 
     private void computeSLO(String key, Collection<AccessLogEntry> logEntries) {
@@ -77,15 +91,21 @@ public final class SLOBasedDetector extends AnomalyDetector {
 
     public static class Builder extends AnomalyDetectorBuilder<SLOBasedDetector, Builder> {
 
-        private int historyLength = 60;
+        private int historyLengthInSeconds = 60 * 60;
+        private int samplingIntervalInSeconds = 60;
         private int responseTimeUpperBound;
         private double sloPercentage = 95.0;
 
         private Builder() {
         }
 
-        public Builder setHistoryLength(int historyLength) {
-            this.historyLength = historyLength;
+        public Builder setHistoryLengthInSeconds(int historyLengthInSeconds) {
+            this.historyLengthInSeconds = historyLengthInSeconds;
+            return this;
+        }
+
+        public Builder setSamplingIntervalInSeconds(int samplingIntervalInSeconds) {
+            this.samplingIntervalInSeconds = samplingIntervalInSeconds;
             return this;
         }
 
