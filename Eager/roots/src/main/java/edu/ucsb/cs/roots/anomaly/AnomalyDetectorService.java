93781a3f0d0d54c23cd8ada1148c0f3b4ca0a381
hiranya <hiranya911@gmail.com>
2016-03-23 21:31:32 -0700

    Merged anomaly detector scheduler into the service

--- Eager/roots/src/main/java/edu/ucsb/cs/roots/anomaly/AnomalyDetectorService.java
+++ Eager/roots/src/main/java/edu/ucsb/cs/roots/anomaly/AnomalyDetectorService.java
@@ -5,7 +5,8 @@ import edu.ucsb.cs.roots.ManagedService;
 import edu.ucsb.cs.roots.RootsEnvironment;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
-import org.quartz.SchedulerException;
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -15,25 +16,29 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.ConcurrentHashMap;
 
+import static com.google.common.base.Preconditions.checkState;
+
 public class AnomalyDetectorService extends ManagedService {
 
-    private final AnomalyDetectorScheduler scheduler;
+    private static final String ANOMALY_DETECTOR_GROUP = "anomaly-detector";
+
+    private final Scheduler scheduler;
     private final Map<String,AnomalyDetector> detectors = new ConcurrentHashMap<>();
 
     public AnomalyDetectorService(RootsEnvironment environment) throws SchedulerException {
         super(environment);
-        this.scheduler = new AnomalyDetectorScheduler(environment.getId());
+        this.scheduler = initScheduler();
     }
 
     public synchronized void doInit() throws Exception {
-        scheduler.init();
+        scheduler.start();
 
         File detectorsDir = new File("conf", "detectors");
         Collection<File> children = FileUtils.listFiles(detectorsDir,
                 new String[]{"properties"}, false);
         children.stream().forEach(f -> {
             try {
-                buildDetector(f);
+                scheduleDetector(f);
             } catch (IOException | SchedulerException e) {
                 log.warn("Error while loading detector from: {}", f.getAbsolutePath(), e);
             }
@@ -44,15 +49,54 @@ public class AnomalyDetectorService extends ManagedService {
         ImmutableList.copyOf(detectors.values()).forEach(this::cancelDetector);
         detectors.clear();
         try {
-            scheduler.destroy();
+            scheduler.shutdown(true);
         } catch (SchedulerException e) {
             log.warn("Error while stopping the scheduler");
         }
     }
 
+    private void schedule(AnomalyDetector detector) throws SchedulerException {
+        JobDetail jobDetail = JobBuilder.newJob(AnomalyDetectorJob.class)
+                .withIdentity(getJobKey(detector.getApplication()))
+                .build();
+        JobDataMap jobDataMap = jobDetail.getJobDataMap();
+        jobDataMap.put(AnomalyDetectorJob.ANOMALY_DETECTOR_INSTANCE, detector);
+
+        Trigger trigger = TriggerBuilder.newTrigger()
+                .withIdentity(getTriggerKey(detector.getApplication()))
+                .withSchedule(SimpleScheduleBuilder.simpleSchedule()
+                        .withMisfireHandlingInstructionIgnoreMisfires()
+                        .repeatForever().withIntervalInSeconds(detector.getPeriodInSeconds()))
+                .startNow()
+                .build();
+        scheduler.scheduleJob(jobDetail, trigger);
+    }
+
+    private JobKey getJobKey(String application) {
+        return JobKey.jobKey(application + "-job", ANOMALY_DETECTOR_GROUP);
+    }
+
+    private TriggerKey getTriggerKey(String application) {
+        return TriggerKey.triggerKey(application + "-job", ANOMALY_DETECTOR_GROUP);
+    }
+
+    private Scheduler initScheduler() throws SchedulerException {
+        StdSchedulerFactory factory = new StdSchedulerFactory();
+        String instanceName = environment.getId() + "-anomaly-detector-scheduler";
+        Properties properties = new Properties();
+        properties.setProperty(StdSchedulerFactory.PROP_SCHED_INSTANCE_NAME, instanceName);
+        properties.setProperty(StdSchedulerFactory.PROP_THREAD_POOL_CLASS,
+                "org.quartz.simpl.SimpleThreadPool");
+        properties.setProperty("org.quartz.threadPool.threadCount", "10");
+        factory.initialize(properties);
+        checkState(factory.getScheduler(instanceName) == null,
+                "Attempting to reuse existing Scheduler");
+        return factory.getScheduler();
+    }
+
     private void cancelDetector(AnomalyDetector detector) {
         try {
-            scheduler.cancel(detector);
+            scheduler.unscheduleJob(getTriggerKey(detector.getApplication()));
             detectors.remove(detector.getApplication());
             log.info("Cancelled detector job for: {}", detector.getApplication());
         } catch (SchedulerException e) {
@@ -60,17 +104,32 @@ public class AnomalyDetectorService extends ManagedService {
         }
     }
 
-    private void buildDetector(File file) throws IOException, SchedulerException {
+    private void scheduleDetector(File file) throws IOException, SchedulerException {
+        Properties properties = new Properties();
         try (FileInputStream in = FileUtils.openInputStream(file)) {
-            log.info("Loading detector from: {}", file.getAbsolutePath());
-            Properties properties = new Properties();
+            log.info("Loading detector configuration from: {}", file.getAbsolutePath());
             properties.load(in);
-            String application = FilenameUtils.removeExtension(file.getName());
-            AnomalyDetector detector = AnomalyDetectorFactory.create(environment, application, properties);
-            scheduler.schedule(detector);
-            detectors.put(application, detector);
-            log.info("Scheduled detector job for: {}", detector.getApplication());
         }
+
+        String application = FilenameUtils.removeExtension(file.getName());
+        AnomalyDetector detector = AnomalyDetectorFactory.create(environment,
+                application, properties);
+        JobDetail jobDetail = JobBuilder.newJob(AnomalyDetectorJob.class)
+                .withIdentity(getJobKey(detector.getApplication()))
+                .build();
+        JobDataMap jobDataMap = jobDetail.getJobDataMap();
+        jobDataMap.put(AnomalyDetectorJob.ANOMALY_DETECTOR_INSTANCE, detector);
+
+        Trigger trigger = TriggerBuilder.newTrigger()
+                .withIdentity(getTriggerKey(detector.getApplication()))
+                .withSchedule(SimpleScheduleBuilder.simpleSchedule()
+                        .withMisfireHandlingInstructionIgnoreMisfires()
+                        .repeatForever().withIntervalInSeconds(detector.getPeriodInSeconds()))
+                .startNow()
+                .build();
+        scheduler.scheduleJob(jobDetail, trigger);
+        detectors.put(application, detector);
+        log.info("Scheduled detector job for: {}", detector.getApplication());
     }
 
 }
