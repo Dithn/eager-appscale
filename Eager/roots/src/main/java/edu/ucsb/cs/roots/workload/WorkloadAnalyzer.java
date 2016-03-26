cbb15b0122610fb297f4cecd7f289c629e190ed0
hiranya <hiranya911@gmail.com>
2016-03-25 17:48:53 -0700

    Further tinckering the workload analyzer

--- Eager/roots/src/main/java/edu/ucsb/cs/roots/workload/WorkloadAnalyzer.java
+++ Eager/roots/src/main/java/edu/ucsb/cs/roots/workload/WorkloadAnalyzer.java
@@ -15,6 +15,7 @@ import org.slf4j.LoggerFactory;
 import java.util.Arrays;
 import java.util.List;
 
+import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 public final class WorkloadAnalyzer {
@@ -45,20 +46,17 @@ public final class WorkloadAnalyzer {
                 return;
             }
 
-            int[] changePoints = detectChangePoints(summary);
-            if (changePoints[0] == -1) {
+            Segment[] segments = detectChangePoints(summary);
+            if (segments == null) {
                 return;
             }
-
-            double[] segments = computeSegments(changePoints, summary);
             for (int i = 1; i < segments.length; i++) {
-                double percentageIncrease = (segments[i] - segments[i-1])*100.0 / segments[i-1];
-                if (percentageIncrease > 50) {
-                    log.info("Problematic workload increase at {}: {} --> {}", changePoints[i-1],
-                            segments[i-1], segments[i]);
-                } else {
-                    log.info("Workload change at {}: {} --> {}", changePoints[i-1],
-                            segments[i-1], segments[i]);
+                double percentageIncrease = (segments[i].mean - segments[i-1].mean)*100.0 /
+                        segments[i-1].mean;
+                // TODO: Find a more meaningful day to handle this information
+                if (percentageIncrease > 200) {
+                    log.info("Problematic workload increase at {}: {} --> {}", segments[i].start,
+                            segments[i-1].mean, segments[i].mean);
                 }
             }
         } catch (Exception e) {
@@ -66,17 +64,21 @@ public final class WorkloadAnalyzer {
         }
     }
 
-    private int[] detectChangePoints(List<Double> data) throws Exception {
+    private Segment[] detectChangePoints(List<Double> data) throws Exception {
+        int[] changePoints;
         try (RClient r = new RClient(environment.getRService())) {
-            r.assign("x", Doubles.toArray(data));
+            double[] values = Doubles.toArray(data);
+            log.info("DATA: {}", Arrays.toString(values));
+            r.assign("x", values);
             r.evalAndAssign("result", "cpt.mean(x, method='PELT')");
-            REXP changePoints = r.eval("cpts(result)");
-            return Arrays.stream(changePoints.asIntegers()).map(i -> i - 1).toArray();
+            REXP result = r.eval("cpts(result)");
+            changePoints = Arrays.stream(result.asIntegers()).map(i -> i - 1).toArray();
         }
-    }
 
-    private double[] computeSegments(int[] changePoints, List<Double> data) {
-        double[] segments = new double[changePoints.length + 1];
+        if (changePoints[0] == -1) {
+            return null;
+        }
+        Segment[] segments = new Segment[changePoints.length + 1];
         int prevIndex = 0;
         for (int i = 0; i <= changePoints.length; i++) {
             int cp;
@@ -85,11 +87,30 @@ public final class WorkloadAnalyzer {
             } else {
                 cp = changePoints[i] + 1;
             }
-            segments[i] = data.subList(prevIndex, cp).stream().mapToDouble(Double::doubleValue)
-                    .average().getAsDouble();
+            segments[i] = new Segment(prevIndex, cp, data);
             prevIndex = cp;
         }
         return segments;
+    }
+
+    private static class Segment {
+        private final int start;
+        private final int end;
+        private final double mean;
+
+        public Segment(int start, int end, List<Double> data) {
+            checkArgument(start < end);
+            log.info("Segment range: {}-{}", start, end - 1);
+            this.start = start;
+            this.end = end;
+            this.mean = data.subList(start, end).stream().mapToDouble(Double::doubleValue)
+                    .average().getAsDouble();
+        }
+
+        int length() {
+            return end - start;
+        }
+
     }
 
 }
