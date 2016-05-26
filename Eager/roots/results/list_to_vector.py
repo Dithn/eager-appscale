7aabb151a81fa6547e6f3132e79c564ed7a32e4d
hiranya <hiranya911@gmail.com>
2016-05-25 22:22:22 -0700

    Improved the vectorize script

--- Eager/roots/results/list_to_vector.py
+++ Eager/roots/results/list_to_vector.py
@@ -1,7 +1,20 @@
+import re
 import sys
+
+def parse_line(line):
+    if re.search(r'\s', line):
+        numbers = line.split()
+        return [ int(number) for number in numbers ]
+    else:
+        return int(line)
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     lines = input.split('\n')
-    vector = [ int(line) for line in lines if line ]
-    print vector
+    vector = [ parse_line(line.strip()) for line in lines if line ]
+    multi_item_input = [ item for item in vector if isinstance(item, list) ]
+    if multi_item_input:
+        for item in vector:
+            print str(item).translate(None, '[,]')
+    else:
+        print vector
