11aa7f62f5a8cfecd92f6c45ce25a19af1c9da42
hiranya <hiranya911@gmail.com>
2016-06-23 18:54:18 -0700

    Adding Bottleneck class

---
+++ Eager/roots/core/src/main/java/edu/ucsb/cs/roots/bi/Bottleneck.java
@@ -0,0 +1,31 @@
+package edu.ucsb.cs.roots.bi;
+
+import com.google.common.base.Strings;
+
+import java.util.Date;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+public final class Bottleneck {
+
+    private final String apiCall;
+    private final Date onsetTime;
+
+    public Bottleneck(String apiCall, Date onsetTime) {
+        checkArgument(!Strings.isNullOrEmpty(apiCall), "API call is required");
+        this.apiCall = apiCall;
+        this.onsetTime = onsetTime;
+    }
+
+    public Bottleneck(String apiCall) {
+        this(apiCall, null);
+    }
+
+    public String getApiCall() {
+        return apiCall;
+    }
+
+    public Date getOnsetTime() {
+        return onsetTime;
+    }
+}
--- Eager/roots/core/src/main/java/edu/ucsb/cs/roots/bi/RelativeImportanceHistoryAnalyzer.java
+++
@@ -1,7 +0,0 @@
-package edu.ucsb.cs.roots.bi;
-
-/**
- * Created by hiranya on 6/23/16.
- */
-public class RelativeImportanceHistoryAnalyzer {
-}
