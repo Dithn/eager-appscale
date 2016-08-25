af9426ecb861a48a3cf95ce108a580b1231b263d
hiranya <hiranya911@gmail.com>
2016-08-25 16:28:59 -0700

    Caching app

---
+++ Eager/multi-path-app/src/main/java/edu/ucsb/cs/CachingStudentServlet.java
@@ -0,0 +1,52 @@
+package edu.ucsb.cs;
+
+import com.google.appengine.api.datastore.*;
+import com.google.appengine.api.memcache.Expiration;
+import com.google.appengine.api.memcache.MemcacheService;
+import com.google.appengine.api.memcache.MemcacheServiceFactory;
+import com.google.appengine.repackaged.com.google.api.client.util.Strings;
+import com.google.appengine.repackaged.com.google.gson.Gson;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static com.google.appengine.repackaged.com.google.api.client.util.Preconditions.checkArgument;
+
+public class CachingStudentServlet extends HttpServlet {
+
+    private static final Gson gson = new Gson();
+
+    @Override
+    protected void doGet(HttpServletRequest req,
+                         HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("id");
+        checkArgument(!Strings.isNullOrEmpty(id));
+
+        MemcacheService cache = MemcacheServiceFactory.getMemcacheService();
+        Student student = (Student) cache.get(id);
+        if (student == null) {
+            DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+            Key key = KeyFactory.createKey("Student", id);
+            try {
+                Entity entity = datastore.get(key);
+                student = new Student();
+                student.setFirstName((String) entity.getProperty("firstName"));
+                student.setLastName((String) entity.getProperty("lastName"));
+                student.setStudentId(id);
+                cache.put(id, student, Expiration.byDeltaSeconds(60));
+            } catch (EntityNotFoundException e) {
+                resp.sendError(404);
+            }
+        }
+
+        resp.setContentType("application/json");
+        ServletOutputStream outputStream = resp.getOutputStream();
+        outputStream.print(gson.toJson(student));
+        outputStream.flush();
+        outputStream.close();
+    }
+}
