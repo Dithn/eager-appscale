d09c50d9f54ae56e496e73c92cd7001fd03519d4
hiranya <hiranya911@gmail.com>
2016-08-31 17:53:39 -0700

    Updated diagram for time line

--- Eager/roots/results/time_line.R
+++ Eager/roots/results/time_line.R
@@ -8,13 +8,13 @@ end <- as.POSIXct('2016-08-19 22:30:00', format='%Y-%m-%d %H:%M:%S')
 range_times <- c(start, end)
 ticks <- seq(start, end, by = "hours")
 
-par(mar=c(5,1,1,1))
+par(mar=c(4,1,1,1))
 plot(NA, ylim=c(0,1), xlim=range_times, ann=FALSE, axes=FALSE, xlab='Time')
-abline(h=0, lwd=2, col="#5B7FA3")
+#abline(h=0, lwd=2, col="#5B7FA3")
 
-title(xlab='Time (HH:mm)', cex.lab=0.75)
+title(xlab='Time (HH:mm)', cex.lab=0.7, line=2)
  
-axis.POSIXct(1, at=seq.POSIXt(range_times[1], range_times[2], by="hours"), format="%H:%M", cex.axis=0.6, pos=0, lwd=0, lwd.tick=2, col="#5B7FA3", font=2, xlab='Time')
+axis.POSIXct(1, at=seq.POSIXt(range_times[1], range_times[2], by=30 * 60), srt=45, format="%H:%M", cex.axis=0.6, pos=0, lwd=2.2, lwd.tick=2, col="#5B7FA3", font=2, xlab='Time')
 
 for (hour in c(12:22)) {
     print(hour)
@@ -27,10 +27,8 @@ for (hour in c(12:22)) {
 
 for (i in c(1:nrow(ts_data))) {
     point <- as.POSIXct(ts_data[i, 1], format='%Y-%m-%d %H:%M:%S')
-    if (ts_data[i,2] == 1) {
-       segments(point, 0, point, 1, col='blue')
-       #points(point, 0.1, type='o', pch='+')
-    } else {
-       segments(point, 0, point, 1, col='green')
+    segments(point, 0, point, 1, col='blue')
+    if (ts_data[i,2] != 1) {
+       points(point, 0.5, type='o')
     }
 }
