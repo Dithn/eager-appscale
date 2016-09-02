592cd5f9b9f66b778201266b5711a2e34b3fd456
hiranya <hiranya911@gmail.com>
2016-09-02 14:34:42 -0700

    Updated time line plotter

--- Eager/roots/results/time_line.R
+++ Eager/roots/results/time_line.R
@@ -1,34 +1,33 @@
 ts_data <- read.table('times.txt', sep=',')
 times <- as.POSIXct(ts_data[,1], format='%Y-%m-%d %H:%M:%S')
 
-dev.new(width=6, height=2)
+dev.new(width=6, height=1.6)
 
-start <- as.POSIXct('2016-08-19 12:30:00', format='%Y-%m-%d %H:%M:%S')
-end <- as.POSIXct('2016-08-19 22:30:00', format='%Y-%m-%d %H:%M:%S')
+start <- as.POSIXct('2016-08-20 01:00:00', format='%Y-%m-%d %H:%M:%S')
+end <- as.POSIXct('2016-08-20 11:00:00', format='%Y-%m-%d %H:%M:%S')
 range_times <- c(start, end)
 ticks <- seq(start, end, by = "hours")
 
-par(mar=c(4,1,1,1))
-plot(NA, ylim=c(0,1), xlim=range_times, ann=FALSE, axes=FALSE, xlab='Time')
+par(mar=c(1.5,0,1,1))
+plot(NA, ylim=c(-0.4,0.4), xlim=range_times, ann=FALSE, axes=FALSE, xlab='Time')
 #abline(h=0, lwd=2, col="#5B7FA3")
 
-title(xlab='Time (HH:mm)', cex.lab=0.7, line=2)
+title(xlab='Time (HH:mm)', cex.lab=0.7, line=0.1)
  
 axis.POSIXct(1, at=seq.POSIXt(range_times[1], range_times[2], by=30 * 60), srt=45, format="%H:%M", cex.axis=0.6, pos=0, lwd=2.2, lwd.tick=2, col="#5B7FA3", font=2, xlab='Time')
 
-for (hour in c(12:22)) {
-    print(hour)
-    start2 <- as.POSIXct(paste('2016-08-19 ', hour, ':00:00', sep=''), format='%Y-%m-%d %H:%M:%S')
-    end2   <- as.POSIXct(paste('2016-08-19 ', hour, ':02:00', sep=''), format='%Y-%m-%d %H:%M:%S')
-    print(start2)
-    print(end2)
-    axis.POSIXct(3, at=seq.POSIXt(start2, end2, by="min"), format="%H:%M", cex.axis=0.6, pos=0, lwd=0, lwd.tick=2, col="red", lab=NA)
+for (hour in c(1:10)) {
+    start2 <- as.POSIXct(paste('2016-08-20 ', hour, ':00:00', sep=''), format='%Y-%m-%d %H:%M:%S')
+    arrows(start2, -0.2, start2, 0, col='red', length=0.1, angle=25)
 }
 
 for (i in c(1:nrow(ts_data))) {
     point <- as.POSIXct(ts_data[i, 1], format='%Y-%m-%d %H:%M:%S')
-    segments(point, 0, point, 1, col='blue')
-    if (ts_data[i,2] != 1) {
-       points(point, 0.5, type='o')
+    print(point)
+    arrows(point, 0.2, point, 0, col='blue', length=0.1, angle=25)
+    if (ts_data[i,2] != 0) {
+       points(point, 0.2, type='o', pch=22, col='blue', bg='blue')
     }
 }
+
+legend('top', legend=c('Fault injection', 'Anomaly detection'), col=c('red', 'blue'), lty=1:1, horiz=T, cex=0.5, bty='n')
