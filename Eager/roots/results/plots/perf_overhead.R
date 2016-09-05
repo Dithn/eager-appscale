2669d89bc323e5af1317bb851ebcd3ee76492ff5
hiranya <hiranya911@gmail.com>
2016-09-05 15:16:47 -0700

    performance overhead test results

--- Eager/roots/results/plots/perf_overhead.R
+++ Eager/roots/results/plots/perf_overhead.R
@@ -3,6 +3,6 @@ barx <- barplot(as.matrix(data), beside=T, xlab='Application', ylab='Average tim
 legend("topright", c("Without Roots", "With Roots"), cex=1, bty="n", fill=c('red','blue'))
 
 red_sd <- c(3.9, 13, 1.5, 2.8)
-blue_sd <- c(3.7, 14.2, 2.2, 3.3)
+blue_sd <- c(3.7, 13.7, 2.2, 3.3)
 arrows(barx[1,], as.numeric(data[1,] - 2*red_sd), barx[1,], as.numeric(data[1,] + 2*red_sd), lwd = 1.5, angle = 90, code = 3, length = 0.05)
 arrows(barx[2,], as.numeric(data[2,] - 2*blue_sd), barx[2,], as.numeric(data[2,] + 2*blue_sd), lwd = 1.5, angle = 90, code = 3, length = 0.05)
